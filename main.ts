a251d28f71ce3afba692bcbefeb817692e7dad90
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-04-17 22:56:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(75)
+    basic.pause(1000)
+    servos.P0.setAngle(180)
+})
 radio.onReceivedValue(function (name, value) {
     if (value == 1) {
-        servos.P0.setAngle(0)
-        basic.pause(500)
-        servos.P0.setAngle(180)
+    	
     }
 })
+servos.P0.setAngle(180)
 radio.setGroup(11)
 radio.setTransmitPower(7)
